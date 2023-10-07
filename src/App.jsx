8d90df72dcffa6f2a6bfb5c93c206162d635130c
Aedan Evangelista <aedantiu@gmail.com>
2023-10-08 01:45:31 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import luxurySVG from "./assets/images/icon-luxury.svg";
 function App() {
   return (
     <>
-      <main className="min-h-screen min-w-screen bg-[#f2f2f2] flex flex-col justify-center items-center">
-        <div className="w-[30rem] max-w-screen mx-auto lg:m-[4rem] items-center flex flex-col lg:flex-row lg:h-[30rem] lg:w-[60rem]">
-          <section className="w-full  lg:h-full lg:w-1/3 bg-[#e28525] text-white flex flex-col justify-evenly rounded-tl-xl rounded-tr-xl lg:rounded-tr-none lg:rounded-tl-xl lg:rounded-bl-xl">
-            <div className="flex flex-col h-full mx-16 justify-between sm:my-8 ">
+      <main className="p-2 min-h-screen min-w-screen bg-[#f2f2f2] flex flex-col justify-center items-center">
+        <div className="lg:m-[4rem] items-center flex flex-col lg:flex-row lg:h-[30rem] lg:w-[60rem]">
+          <section className="w-full lg:h-full lg:w-1/3 bg-[#e28525] text-white flex flex-col justify-evenly rounded-tl-xl rounded-tr-xl lg:rounded-tr-none lg:rounded-tl-xl lg:rounded-bl-xl">
+            <div className="flex flex-col h-full mx-[2rem] mx-16 justify-between sm:my-8 ">
               <img src={sedanSVG} className="w-[5rem] my-8" />
 
               <h1 className="font-semibold text-5xl font-Sans my-4">SEDANS</h1>
-              <p className="opacity-80 lg:h-[9rem]">
+              <p className="opacity-80 text-[15px]">
                 Choose a sedan for its affordability and excellent fuel economy.
                 Ideal for cruising in the city or on your next road trip.
               </p>
@@ -26,7 +26,7 @@ function App() {
             <div className="flex flex-col h-full mx-16 justify-between sm:my-8 ">
               <img src={suvSVG} className="w-[5rem] my-8" />
               <h1 className="font-semibold text-5xl font-Sans my-4">SUVS</h1>
-              <p className="opacity-80 lg:h-[9rem]">
+              <p className="opacity-80 text-[15px]">
                 Take an SUV for its spacious interior, power, and versatility.
                 Perfect for your next family vacation and off-road adventures.
               </p>
@@ -39,7 +39,7 @@ function App() {
             <div className="flex flex-col h-full mx-16 justify-between sm:my-8 ">
               <img src={luxurySVG} className="w-[5rem] my-8" />
               <h1 className="font-semibold text-5xl font-Sans my-4">LUXURY</h1>
-              <p className="opacity-80 lg:h-[9rem]">
+              <p className="opacity-80 text-[15px]">
                 Cruise in the best car brands without the bloated prices. Enjoy
                 the enhanced comfort of a luxury rental and arrive in style.
               </p>
