d08245b2aa9d99b4ef85a366c58d2066b52dd879
Aedan Evangelista <aedantiu@gmail.com>
2023-10-07 16:53:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
               <h1 className="font-semibold lg:text-5xl text-[6rem] font-Sans my-4">
                 SEDANS
               </h1>
-              <p className="opacity-80 lg:text-base text-[3rem]">
+              <p className="opacity-80 lg:text-base text-[2.5rem]">
                 Choose a sedan for its affordability and excellent fuel economy.
                 Ideal for cruising in the city or on your next road trip.
               </p>
@@ -30,7 +30,7 @@ function App() {
               <h1 className="font-semibold lg:text-5xl text-[6rem] font-Sans my-4">
                 SUVS
               </h1>
-              <p className="opacity-80 lg:text-base text-[3rem]">
+              <p className="opacity-80 lg:text-base text-[2.5rem]">
                 Take an SUV for its spacious interior, power, and versatility.
                 Perfect for your next family vacation and off-road adventures.
               </p>
@@ -45,7 +45,7 @@ function App() {
               <h1 className="font-semibold lg:text-5xl font-Sans my-4 text-[6rem]">
                 LUXURY
               </h1>
-              <p className="opacity-80 lg:text-base text-[3rem]">
+              <p className="opacity-80 lg:text-base text-[2.5rem]">
                 Cruise in the best car brands without the bloated prices. Enjoy
                 the enhanced comfort of a luxury rental and arrive in style.
               </p>
